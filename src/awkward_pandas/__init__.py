7b174852ee9a1e760b6d7a53527c5639e07058ea
Doug Davis <ddavis@ddavis.io>
2022-07-07 15:34:09 -0400

    First

---
+++ src/awkward_pandas/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from awkward_pandas._version import version as __version__
---
+++ tests/test_package.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import awkward_pandas as akp
+
+
+def test_version():
+    assert akp.__version__
