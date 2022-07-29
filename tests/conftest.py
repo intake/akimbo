2d0d4dde0b5843ecee12548d7a971edc2ce760a0
Doug Davis <ddavis@ddavis.io>
2022-07-29 12:16:18 -0500

    another fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,17 @@ def data_missing(data):
     return type(data)._from_sequence([None, data[0]])
 
 
+@pytest.fixture(params=["data", "data_missing"])
+def all_data(request, data, data_missing):
+    """Parametrized fixture returning 'data' or 'data_missing' integer arrays.
+    Used to test dtype conversion with and without missing values.
+    """
+    if request.param == "data":
+        return data
+    elif request.param == "data_missing":
+        return data_missing
+
+
 @pytest.fixture
 def na_value():
     return np.nan
