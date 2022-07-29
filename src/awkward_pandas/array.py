0f0205715f0c828d201ceb4d16288e3155086213
Martin Durant <martindurant@users.noreply.github.com>
2022-07-29 13:08:24 -0400

    WIP: add binop implementation (#6)
    * add binop implementation
    
    * remove extra
    
    * add reducer

--- src/awkward_pandas/array.py
+++ src/awkward_pandas/array.py
@@ -7,7 +7,8 @@ from typing import Any, Literal
 import awkward._v2 as ak
 import numpy as np
 import pandas as pd
-from pandas.core.arrays.base import ExtensionArray, ExtensionScalarOpsMixin
+from pandas.core.arrays.base import ExtensionArray, ExtensionScalarOpsMixin, set_function_name
+from pandas.core.dtypes.generic import ABCSeries, ABCIndex, ABCDataFrame
 
 from awkward_pandas.dtype import AwkwardDtype
 
@@ -55,10 +56,24 @@ class AwkwardArray(ExtensionArray, ExtensionScalarOpsMixin):
         for i in range(len(self)):
             yield self._data[i]
 
-    def __eq__(self, other):
-        if isinstance(other, AwkwardArray):
-            return type(self)(self._data == other._data)
-        return self == type(self)(other)
+    @classmethod
+    def _create_method(cls, op, coerce_to_dtype=True, result_dtype=None):
+
+        def _binop(self, other):
+            if isinstance(other, (ABCSeries, ABCIndex, ABCDataFrame)):
+                # rely on pandas to unbox and dispatch to us
+                return NotImplemented
+
+            lvalues = self
+            rvalues = cls(other)
+
+            return cls(op(lvalues._data, rvalues._data))
+
+        op_name = f"__{op.__name__}__"
+        return set_function_name(_binop, op_name, cls)
+
+    def _reduce(self, name: str, *, skipna: bool = True, **kwargs):
+        return getattr(ak, name)(self._data, **kwargs)
 
     @property
     def dtype(self):
