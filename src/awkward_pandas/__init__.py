feababdb6cfc12a14b5dcb4159589d95d06280fe
Doug Davis <ddavis@ddavis.io>
2022-08-23 11:46:39 -0500

    use hatch

--- src/awkward_pandas/__init__.py
+++ src/awkward_pandas/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import awkward_pandas.accessor
-from awkward_pandas._version import version as __version__
+from awkward_pandas.version import version as __version__
 from awkward_pandas.array import AwkwardExtensionArray, merge
 from awkward_pandas.dtype import AwkwardDtype
 from awkward_pandas.io import read_parquet
