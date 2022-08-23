f3f6d54236f497b09ac19010f89f85e0bd255905
Doug Davis <ddavis@ddavis.io>
2022-08-23 11:48:45 -0500

    linting

--- src/awkward_pandas/__init__.py
+++ src/awkward_pandas/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-import awkward_pandas.accessor
-from awkward_pandas.version import version as __version__
+import awkward_pandas.accessor  # noqa
 from awkward_pandas.array import AwkwardExtensionArray, merge
 from awkward_pandas.dtype import AwkwardDtype
 from awkward_pandas.io import read_parquet
+from awkward_pandas.version import version as __version__  # noqa
 
 __all__ = ("AwkwardDtype", "AwkwardExtensionArray", "read_parquet", "merge")
