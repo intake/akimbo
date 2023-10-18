3435d8508ae65a2ae3f541ba1287561d041f6fcd
Doug Davis <ddavis@anaconda.com>
2023-10-18 13:18:06 -0500

    support args and kwargs

--- src/awkward_pandas/datetimes.py
+++ src/awkward_pandas/datetimes.py
@@ -13,7 +13,17 @@ if TYPE_CHECKING:
     from awkward_pandas.accessor import AwkwardAccessor
 
 
-component_extraction = [
+PYARROW_FUNCTIONS = [
+    # CONVERSIONS
+    "cast",
+    "ceil_temporal",
+    "floor_temporal",
+    "round_temporal",
+    "run_end_decode",
+    "run_end_encode",
+    "strftime",
+    "strptime    ",
+    # COMPONENT_EXTRACTION
     "day",
     "day_of_week",
     "day_of_year",
@@ -35,10 +45,7 @@ component_extraction = [
     "week",
     "year",
     "year_month_day",
-]
-
-
-differences = [
+    # DIFFERENCES
     "day_time_interval_between",
     "days_between",
     "hours_between",
@@ -59,8 +66,11 @@ class DatetimeAccessor:
     def __init__(self, accessor: AwkwardAccessor) -> None:
         self.accessor = accessor
 
+    def __dir__(self) -> list[str]:
+        return sorted(PYARROW_FUNCTIONS)
+
     def __getattr__(self, attr: str) -> Any:
-        if not (attr in component_extraction or attr in differences):
+        if attr not in PYARROW_FUNCTIONS:
             raise ValueError
 
         fn = getattr(pc, attr)
@@ -68,8 +78,17 @@ class DatetimeAccessor:
         @functools.wraps(fn)
         def wrapper(*args, **kwargs):
             arrow_array = ak.to_arrow(self.accessor.array, extensionarray=False)
-            result = ak.from_arrow(fn(arrow_array))
+
+            arrow_args, arrow_kwargs = [], {}
+            for arg in args:
+                if isinstance(arg, pd.Series) and arg.dtype == "awkward":
+                    arrow_args.append(ak.to_arrow(arg.ak.array, extensionarray=False))
+            for k, v in kwargs.items():
+                if isinstance(v, pd.Series) and v.dtype == "awkward":
+                    arrow_kwargs[k] = ak.to_arrow(v.ak.array, extensionarray=False)
+
+            result = fn(arrow_array, *arrow_args, **arrow_kwargs)
             idx = self.accessor._obj.index
-            return pd.Series(AwkwardExtensionArray(result), index=idx)
+            return pd.Series(AwkwardExtensionArray(ak.from_arrow(result)), index=idx)
 
         return wrapper
