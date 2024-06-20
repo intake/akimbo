68ac4d869f9e370f3a84a1f8b98c8b17f4fd61eb
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-06-20 16:44:04 -0400

    add imort

--- src/akimbo/__init__.py
+++ src/akimbo/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import akimbo.mixin as mixin
 from akimbo.io import read_json, read_parquet
 from akimbo.version import version as __version__  # noqa
 
 __all__ = (
+    "mixin",
     "read_parquet",
     "read_json",
 )
