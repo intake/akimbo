ca44cd31ad8e71f26ba7180584560d8dc306804a
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-07-25 15:51:06 -0400

    Revamp IO

--- src/akimbo/__init__.py
+++ src/akimbo/__init__.py
@@ -5,12 +5,14 @@ from awkward import behavior, mixin_class, mixin_class_method  # re-export
 import akimbo.datetimes as datetimes
 import akimbo.mixin as mixin
 import akimbo.strings as strings
-from akimbo.io import read_json, read_parquet
+from akimbo.io import join, read_avro, read_json, read_parquet
 from akimbo.version import version as __version__  # noqa
 
 __all__ = (
     "datetimes",
     "mixin",
+    "join",
+    "read_avro",
     "read_parquet",
     "read_json",
     "behavior",
--- src/akimbo/io.py
+++ src/akimbo/io.py
@@ -3,16 +3,36 @@ from __future__ import annotations
 import awkward as ak
 import fsspec
 
-import akimbo.pandas
+
+def ak_to_series(ds, backend="pandas", extract=True):
+    if backend == "pandas":
+        import akimbo.pandas
+
+        s = akimbo.pandas.PandasAwkwardAccessor._to_output(ds)
+    elif backend == "polars":
+        import akimbo.polars
+
+        s = akimbo.polars.PolarsAwkwardAccessor._to_output(ds)
+    elif backend == "dask":
+        import akimbo.dask
+
+        # TODO: actually don't use this, use dask-awkward, or dask.dataframe
+        s = akimbo.polars.PolarsAwkwardAccessor._to_output(ds)
+    else:
+        raise ValueError("Backend must be in {'pandas', 'polars', 'dask'}")
+    if extract:
+        return s.ak.unmerge()
+    return s
 
 
 def read_parquet(
     url: str,
     storage_options: dict | None = None,
     extract: bool = True,
+    backend: str = "pandas",
     **kwargs,
 ):
-    """Read a Parquet dataset with nested data into a pandas Series or DataFrame.
+    """Read a Parquet dataset with nested data into a Series or DataFrame.
 
     This may cope with some deeply nested structures that pandas refuses
     to read by itself.
@@ -24,44 +44,109 @@ def read_parquet(
     Parameters
     ----------
     url: data location
+        Directory with data files, single file or glob pattern
     storage_options: any arguments for an fsspec backend
     extract: whether to turn top-level records into a dataframe. If False,
         will return a series.
+    backend: one of "pandas", "polars" or "dask"
     """
+    # TODO: is this useful compared to pyarrow.parquet? Describe differences
     ds = ak.from_parquet(url, storage_options=storage_options, **kwargs)
-    s = akimbo.pandas.PandasAwkwardAccessor._to_output(ds)
-    if extract:
-        return s.ak.unmerge()
-    return s
+    return ak_to_series(ds, backend, extract=extract)
 
 
 def read_json(
-    url,
+    url: str,
     storage_options: dict | None = None,
-    extract=True,
+    extract: bool = True,
+    backend: str = "pandas",
     **kwargs,
 ):
-    """Read a JSON dataset with nested data into a pandas Series or DataFrame.
+    """Read a JSON dataset with nested data into a Series or DataFrame.
 
     You can pass a selection of columns to read (list or jsonschema format),
     using ``schema=``, and
     other columns will not be parsed into memory. See the docs for
     ak.from_json for further details.
 
+    (examples to come)
+
     Parameters
     ----------
-    url: data location
+    url: data location (may include glob characters)
     storage_options: any arguments for an fsspec backend
     extract: whether to turn top-level records into a dataframe. If False,
         will return a series.
+    backend: one of "pandas", "polars" or "dask"
     """
-    with fsspec.open(url, **storage_options) as f:
-        ds = ak.from_json(
-            f,
-            line_delimited=True,
-            **kwargs,
-        )
-    s = akimbo.pandas.PandasAwkwardAccessor._to_output(ds)
-    if extract:
-        return s.ak.unmerge()
-    return s
+    with fsspec.open_files(url, **(storage_options or {})) as f:
+        ds = ak.concatenate([ak.from_json(_, line_delimited=True, **kwargs) for _ in f])
+    return ak_to_series(ds, backend, extrcact=extract)
+
+
+def read_avro(
+    url: str,
+    storage_options: dict | None = None,
+    extract: bool = True,
+    backend: str = "pandas",
+    **kwargs,
+):
+    """Read AVRO structured data files
+
+    Parameters
+    ----------
+    url: data location (may include glob characters)
+    storage_options: any arguments for an fsspec backend
+    extract: whether to turn top-level records into a dataframe. If False,
+        will return a series.
+    backend: one of "pandas", "polars" or "dask"
+    """
+    with fsspec.open(url, **(storage_options or {})) as f:
+        ds = ak.from_avro_file(f, **kwargs)
+    return ak_to_series(ds, backend, extract=extract)
+
+
+def _merge(ind1, ind2, builder):
+    """numba jittable laft join/merge index finder"""
+    len2 = len(ind2)
+    j = 0
+    for i in ind1:
+        builder.begin_list()
+        while True:
+            if j >= len2:
+                break
+            if i > ind2[j]:
+                # ID not yet found
+                j += 1
+                continue
+            if i < ind2[j]:
+                # no more entrie
+                break
+            # hit
+            while True:
+                builder.append(j)
+                j += 1
+                if j >= len2 or i != ind2[j]:
+                    break
+        builder.end_list()
+
+
+def join(table1, table2, key, colname, sort=False):
+    """Make nested ORM-style left join on common key in two tables
+
+    Assuming ``key`` is a field in each table, the output will look like
+    ``table1`` but with an extra column ``colname`` containing a list of
+    records from matching rows in ``table2``.
+    """
+    import numba
+
+    if sort:
+        # indexed view is not cache friendly; real sort is better
+        table1 = table1[ak.argsort(table2[key], axis=0)]
+        table2 = table2[ak.argsort(table2[key], axis=0)]
+    merge = numba.njit(cache=True)(_merge)
+    builder = ak.ArrayBuilder()
+    merge(table1[key], table2[key], builder)
+    merge_index = builder.snapshot()
+    # TODO: apply indexing to add field to original table
+    return merge_index
